c52bc488c3ed88b9a921c0c007a4d5b39025b7f4
MohamedMatrab <mb200ts@gmail.com>
2024-12-24 17:48:47 +0100

    added getProducts

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,22 @@
+﻿namespace Catalog.API.Products.GetProducts;
+
+public record GetProductsRequest();
+public record GetProductsResponse(IEnumerable<Product> Products);
+public class GetProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products",async (ISender sender) =>
+        {
+            var ressult = await sender.Send(new GetProductsQuery());
+            var response = ressult.Adapt<GetProductsResponse>();
+
+            return Results.Ok(response) ;
+        })
+        .WithName("GetProducts")
+        .Produces<GetProductsResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Products")
+        .WithDescription("Get Products");
+    }
+}
