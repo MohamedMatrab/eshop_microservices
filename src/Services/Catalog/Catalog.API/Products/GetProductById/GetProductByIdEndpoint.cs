c4821bc20a136ab068d74803836be6e5b0b11007
MohamedMatrab <mb200ts@gmail.com>
2024-12-24 23:56:47 +0100

    changes in catalog.api

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,22 @@
+﻿namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdRequest();
+public record GetProductByIdResponse(Product Product);
+
+public class GetProductByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id:guid}", async (Guid id,ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByIdQuery(id));
+            var response = result.Adapt<GetProductByIdResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("GetProductById")
+        .Produces<GetProductByIdResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Product By ID")
+        .WithDescription("Get Product By ID");
+    }
+}
