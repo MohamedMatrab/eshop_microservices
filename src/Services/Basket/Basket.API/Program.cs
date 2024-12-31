d7a201df7ebcd524c9619fc30590cd786fc39a7e
MohamedMatrab <mb200ts@gmail.com>
2024-12-31 16:24:13 +0100

    added main feautures + implemented repository pattern + docker compose basketdb

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,5 +1,31 @@
+using BuildingBlocks.Exceptions.Handlers;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var assembly = typeof(Program).Assembly;
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(configuration: c =>
+{
+    c.RegisterServicesFromAssembly(assembly);
+    c.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    c.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Default")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository,BasketRepository>();
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
 var app = builder.Build();
+
+app.MapCarter();
+
+app.UseExceptionHandler(options =>{});
 
 app.Run();
--- src/Services/Basket/Basket.API/Usings.cs
+++ src/Services/Basket/Basket.API/Usings.cs
@@ -1,2 +1,11 @@
 ﻿global using Basket.API.Models;
 global using BuildingBlocks.CQRS;
+global using Carter;
+global using Marten;
+global using Mapster;
+global using MediatR;
+global using FluentValidation;
+global using BuildingBlocks.Exceptions;
+global using Basket.API.Exceptions;
+global using Basket.API.Data;
+global using BuildingBlocks.Behaviors;
