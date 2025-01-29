4bd04c5cbd2d14fbd6cf02f00f824c5b45d0b4d3
MohamedMatrab <mb200ts@gmail.com>
2025-01-29 14:32:44 +0100

    setting up ordering service

---
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -0,0 +1,19 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastucture;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services
+    .AddApplication()
+    .AddInfrastructure(builder.Configuration)
+    .AddApiServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.Run();
---
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastucture;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Database");
+        return services;
+    }
+}
